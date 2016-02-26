ebbab104b3b03ccdd95f8f671f245f9b67ce6b4f
SKHameed86 <shahnaz.hameed@outlook.com>
2016-02-26 22:53:41 +0000

    Final Commit
    Made final changes to script

--- script/gol.js
+++ script/gol.js
@@ -19,10 +19,10 @@ module.exports = function(){
 				return 0;
 			}
 		}
-		return survival(cellState, numberOfLiveNeighbours);
+		return survive(cellState, numberOfLiveNeighbours);
 	}
 	
-	function survival(cellState, numberOfLiveNeighbours) {
+	function survive(cellState, numberOfLiveNeighbours) {
 		if (cellState === 1){
 			if (numberOfLiveNeighbours === 2 || numberOfLiveNeighbours === 3) {
 				return 1;
@@ -47,27 +47,57 @@ module.exports = function(){
 		return underpopulation(cellState, numberOfLiveNeighbours);
 	}
 
-	function evolve(gridState){
+	function evolve(gridState, iterations){
 		var initialState = gridState;
 		var newState = [];
 		var mirrorState = []; //array to hold values of initialState before updated to newState
+		var iterations = 0;
+		var cell = null;
+		var index = 0;
 		
 		//Populate mirrorState with values of initialState
-		for (var i = 0; i < initialState.length; i++){
-			for (var j = 0; j < initialState.length[i]; i++){
+		for (var i = 0; i < initialState.length; i++) {
+			for (var j = 0; j < initialState[i].length; i++) {
 				mirrorState.push(initialState[i][j]);
 			}
 		}
 		
+		//Count neighbours near live cell
+			function countNeighbour(i, j) {
+				var x = 0;
+				x += mirrorState[i-1][j-1];
+				x += mirrorState[i-1][j];
+				x += mirrorState[i-1][j+1];
+				x += mirrorState[i][j-1];
+				x += mirrorState[i][j+1];
+				x += mirrorState[i+1][j-1];
+				x += mirrorState[i+1][j];
+				x += mirrorState[i+1][j+1];
+				return x;
+			}
 		
-		
-		return newState;
+		//Inputs values into newState
+		for(var k = 0; k < initialState.length; k++) {
+			for(var l = 0; l < initialState[k].length; l++) {
+				numberOfLiveNeighbours = countNeighbour(i, j)
+				cell = underpopulation(initialState[k][l], mirrorState[index]);
+				newState[k].push(cell);
+				index++;
+			}
+		}
+
+		if(iterations > 0) {
+			return evolve(newState, iterations);
+		}
+			else {
+				return newState;
+			}
 	}
 
 	return {
 		underpopulation: underpopulation,
 		overcrowding: overcrowding,
-		survival: survival,
+		survive: survive,
 		createlife: createlife,
 		barren: barren,
 		evolve: evolve
--- spec/golSpec.js
+++ spec/golSpec.js
@@ -1,7 +1,7 @@
 describe('The rules', function(){
 	var underpopulation = require('../script/gol')().underpopulation;
-	var overcrowding = require('../script/gol')().overcrowding;
-	var survival = require('../script/gol')().survival;
+	var overcrowded = require('../script/gol')().overcrowded;
+	var survive = require('../script/gol')().survive;
 	var createlife = require('../script/gol')().createlife;
 	var barren = require('../script/gol')().barren;
 
@@ -15,27 +15,27 @@ describe('The rules', function(){
 		expect(underpopulation(1,2)).toBe(1);
 	});
 	
-//Scenario 2: Overcrowding
-	it('Any live cell with more than three live neighbours dies, as if caused by overcrowding.', function (){
-		expect(overcrowding(1,4)).toBe(0);
-		expect(overcrowding(1,5)).toBe(0);
-		expect(overcrowding(1,6)).toBe(0);
-		expect(overcrowding(1,7)).toBe(0);
-		expect(overcrowding(1,8)).toBe(0);
-		expect(overcrowding(0,4)).toBe(0);
-		expect(overcrowding(0,5)).toBe(0);
-		expect(overcrowding(0,6)).toBe(0);
-		expect(overcrowding(0,7)).toBe(0);
-		expect(overcrowding(0,8)).toBe(0);
+//Scenario 2: Overcrowded
+	it('Any live cell with more than three live neighbours dies, as if caused by overcrowded.', function (){
+		expect(overcrowded(0,4)).toBe(0);
+		expect(overcrowded(0,5)).toBe(0);
+		expect(overcrowded(0,6)).toBe(0);
+		expect(overcrowded(0,7)).toBe(0);
+		expect(overcrowded(0,8)).toBe(0);
+		expect(overcrowded(1,4)).toBe(0);
+		expect(overcrowded(1,5)).toBe(0);
+		expect(overcrowded(1,6)).toBe(0);
+		expect(overcrowded(1,7)).toBe(0);
+		expect(overcrowded(1,8)).toBe(0);
 	});
 	
-//Scenario 3: Survival
+//Scenario 3: Survive
 	it('Any live cell with two or three live neighbours, stays alive - thus surviving.', function (){
-		expect(survival(1,0)).toBe(0);
-		expect(survival(1,1)).toBe(0);
-		expect(survival(1,2)).toBe(1);
-		expect(survival(1,3)).toBe(1);
-		expect(survival(1,4)).toBe(0);
+		expect(survive(1,0)).toBe(0);
+		expect(survive(1,1)).toBe(0);
+		expect(survive(1,2)).toBe(1);
+		expect(survive(1,3)).toBe(1);
+		expect(survive(1,4)).toBe(0);
 	});
 
 //Scenario 4: Creation of Life
@@ -50,14 +50,13 @@ describe('The rules', function(){
 //Scenario 5: No Live Cells
 	it('With no live cells, the next state also contains no live cells.', function (){
 		expect(barren(0,0)).toBe(0);
-	});
+	});	
 });
-	
-//Scenario 6: Outcomes for Seeded Grid
+
 describe('Game', function (){
 	var evolve = require('../script/gol')().evolve;
 
-	it('can perform a simple evolution.', function(){
+	it('can perform a simple evolution', function(){
 		var initialState = [
 			[0,0,0],
 			[0,1,0],
@@ -70,10 +69,10 @@ describe('Game', function (){
 			[0,0,0]
 		];
 
-		expect(evolve(initialState)).toEqual(resultState);
+		expect(evolve(initialState,1)).toEqual(resultState);
 	});
-
-	it('can die out.', function(){
+	
+		it('can die out.', function(){
 		var initialState = [
 			[1,0,1],
 			[1,1,1],
@@ -86,23 +85,23 @@ describe('Game', function (){
 			[0,0,0]
 		];
 
-		expect(evolve(initialState)).toEqual(resultState);
+		expect(evolve(initialState,1)).toEqual(resultState);
 	});
 
 	it('can beat the odds.', function(){
 		var initialState = [
-			[1,0,0],
-			[0,1,0],
-			[0,0,1]
+			[0,0,0],
+			[1,1,1],
+			[0,0,0]
 		];
 
 		var resultState = [
-			[1,0,0],
 			[0,1,0],
-			[0,0,1]
+			[0,1,0],
+			[0,1,0]
 		];
 
-		expect(evolve(initialState)).toEqual(resultState);
+		expect(evolve(initialState,2)).toEqual(resultState);
 	});
 
 	it('can perform a "miracle of life".', function(){
@@ -118,7 +117,7 @@ describe('Game', function (){
 			[0,1,0]
 		];
 
-		expect(evolve(initialState)).toEqual(resultState);
+		expect(evolve(initialState,2)).toEqual(resultState);
 	});
 
 	it('can remain barren', function(){
@@ -134,6 +133,6 @@ describe('Game', function (){
 			[0,0,0]
 		];
 
-		expect(evolve(initialState)).toEqual(resultState);
+		expect(evolve(initialState,1)).toEqual(resultState);
 	});
 });
